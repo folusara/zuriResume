375c15b92dbbe081676a8ca764b1f3df9d18caf4
Tolulope Aina-Badejo <61244960+folusara@users.noreply.github.com>
2021-08-19 22:33:24 +0100

    first commit

---
+++ action.js
@@ -0,0 +1,11 @@
+function displayName() {
+   
+    let value1 = document.querySelector("#val1");
+    let value2 = document.querySelector("#val2");
+   
+    value1.innerHTML="Tolulope";
+    value2.innerHTML="Aina-Badejo";
+
+}
+
+displayName()
